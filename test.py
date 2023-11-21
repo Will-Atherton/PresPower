e4056cd5e06bf4447ad1dcbf217f468a4ef18c51
Will-Atherton <56821053+Will-Atherton@users.noreply.github.com>
2023-11-21 14:13:51 +0000

    testing code

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-"Start"
+import sys, getopt
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "a:bc", ["argument="])
+except getopt.GetoptError:
+    print("bad argument")
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == "-a":
+        print("testing: " + arg)
+    elif opt == "-b":
+        print("testb")
+    elif opt == "-c":
+        print("testc" + arg)
+    elif opt == "--argument":
+        print("testing long " + arg)
